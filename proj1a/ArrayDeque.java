93264a37056064a96bc1d72a25971fe7f4c1dbbf
unknown <b10901187@g.ntu.edu.tw>
2022-08-12 22:03:24 +0800

    submit proj1a

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -12,34 +12,42 @@ public class ArrayDeque<T> {
         _nextLast = 5;
         _array = (T[]) new Object[8];
     }
-
+    
     public void resize(int capacity){
         T[] replace = (T[]) new Object[capacity];
+        int index = 0;
 
         for(int i = (_nextFirst+1) % _capacity; i != _nextLast; i = (i+1) % _capacity){
-            replace[i] = _array[i];
+            replace[index] = _array[i];
+            // System.out.print(i + " ");
+            // System.out.print(_array[i] + "   ");
+            index++;
         }
 
+        // System.out.println(size()+10);
+        _nextFirst = capacity-1;
+        _nextLast = _size;
         _array = replace;
+        _capacity = capacity;
+
     }
+
     public void addFirst(T item){
-        
-        if(_size == _capacity)  resize(_capacity * 2);
 
         _array[_nextFirst] = item;
         _nextFirst--;
         if(_nextFirst < 0)  _nextFirst = _capacity-1;
         _size++;
+        if(_size == _capacity-2)  resize(_capacity * 2);
     }
 
     public void addLast(T item){
-
-        if(_size == _capacity)  resize(_capacity * 2);
 
         _array[_nextLast] = item;
         _nextLast++;
         if(_nextLast == _capacity)  _nextLast = 0; 
         _size++;
+        if(_size == _capacity-2)  resize(_capacity * 2);
     }
 
     public boolean isEmpty(){
@@ -53,9 +61,9 @@ public class ArrayDeque<T> {
     public void printDeque(){
         
 
-        System.out.println(_nextFirst);
-        System.out.println(_nextLast);
-        System.out.println(_capacity);
+        // System.out.println(_nextFirst);
+        // System.out.println(_nextLast);
+        // System.out.println(_size);
 
         for(int i = (_nextFirst+1) % _capacity; i != _nextLast; i = (i+1) % _capacity){
             // System.out.print(i);
@@ -65,20 +73,20 @@ public class ArrayDeque<T> {
 
     public T removeFirst(){
 
-        if(_size <= _capacity/4)  resize(_capacity * 1/4);
-
         int temp = _nextFirst;
         _nextFirst = (_nextFirst+1)%_size;
+        _size--;
+        if(_size <= _capacity/4)  resize(_capacity / 2);
         return _array[temp];
     }
 
     public T removeLast(){
 
-        if(_size <= _capacity/4)  resize(_capacity * 1/4);
-
         int temp = _nextLast;
         _nextLast = (_nextLast-1);
         if(_nextLast < 0)  _nextLast *= -1;
+        _size--;
+        if(_size <= _capacity/4)  resize(_capacity / 2);
         return _array[temp];
     }
 
@@ -87,15 +95,27 @@ public class ArrayDeque<T> {
         return _array[index];
     }
 
+    // public int getCapacity(){  return _capacity;}
+
     // public static void main(String[] args){
     //     ArrayDeque<Double> a = new ArrayDeque<Double>();
     //     a.addFirst(3.0);
     //     a.addLast(5.0);
-    //     a.printDeque();
+    //     // a.printDeque();
     //     a.addFirst(2.0);
     //     a.addFirst(1.0);
     //     a.addFirst(0.0);
     //     a.addFirst(-1.0);
+        
+    //     a.addFirst(-2.0);
+    //     a.addFirst(-3.0);
+    //     a.removeLast();
+    //     a.removeLast();
+    //     a.removeLast();
+    //     a.removeLast();
+    //     a.removeLast();
+    //     a.removeLast();
     //     a.printDeque();
+    //     // System.out.println(a.getCapacity());
     // }
 }
